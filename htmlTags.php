e1d84eb8692b80d9c839f4cdb0bee661ea3ee265
Tanuka Nayak <tanuka1990@gmail.com>
2017-11-19 02:08:11 -0500

    added closeTable function

--- htmlTags.php
+++ htmlTags.php
@@ -4,13 +4,16 @@ class htmlTags {
         echo "<table cellpadding='5px' border='1px' style='border-collapse: collapse'>";
     }
     static public function tableHeader($text) {
-        echo '<th style="font-size: large">'.$text.'</th>';
+        echo '<th>'.$text.'</th>';
     }
     static public function tableContent($text) {
         echo '<td>'.$text.'</td>';
     }
     static public function breakTableRow() {
         echo '</tr>';
+    }
+    static public function closeTable() {
+        echo '</table>';
     }
 }
 ?>
