013d28e9b4850c5ec06b6555d6b50f21c7026b5d
Tanuka Nayak <tanuka1990@gmail.com>
2017-11-19 02:07:10 -0500

    updated htmlTable class with htmlTags class' functions

--- htmlTable.php
+++ htmlTable.php
@@ -2,20 +2,17 @@
     class htmlTable {
         static function makeTable($record) {
             htmlTags::tableFormat();
-            $html = '<tr>';
-                foreach($record[0] as $key=>$value) {
-                $html .= '<th>' . htmlspecialchars($key) . '</th>';
-                }
-                $html .= '</tr>';
-            foreach($record as $key=>$value) {
-            $html .= '<tr>';
-                foreach($value as $key2=>$value2) {
-                $html .= '<td>' . htmlspecialchars($value2) . '<br></td>';
-                }
-                $html .= '</tr>';
+            foreach($record[0] as $key=>$value) {
+                htmlTags::tableHeader($key);
             }
-            $html .= '</tbody></table>';
-            return print_r($html);
+            htmlTags::breakTableRow();
+            foreach($record as $key=>$value) {
+                foreach($value as $key2=>$value2) {
+                    htmlTags::tableContent($value2);
+                }
+                htmlTags::breakTableRow();
+            }
+            htmlTags::closeTable();
         }
     }
 ?>
