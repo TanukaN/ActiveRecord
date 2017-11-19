90bab7019dab20df23923627bbf5657e504b6399
Tanuka Nayak <tn76@afsconnect1.njit.edu>
2017-11-19 00:28:25 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+//turn on debugging messages
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
+
+    class Manage {						//Defining a class with autoload function to attempt to load undefined class - in this case htmlTags class
+        public static function autoload($class) {
+            include $class . '.php';
+        }
+    }
+    spl_autoload_register(array('Manage', 'autoload'));
+
+    define('SERVERNAME', 'sql2.njit.edu');
+    define('USERNAME','tn76');
+    define('PASSWORD','DblDTPzb');
+    define('DBNAME','tn76');
+
+    class dbConn
+    {
+        protected static $conn;
+        function __construct()
+        {
+            try {
+                self::$conn = new PDO('mysql:host='.SERVERNAME.';dbname='.DBNAME , USERNAME, PASSWORD);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }
+            catch(PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+        }
+        static function getConnection(){
+            if(!self::$conn) {
+                new dbConn;
+            }
+            return self::$conn;
+        }
+    }
+
+    /**********************************ACCOUNTS TABLE***********************************/
+    StringFunctions::printThisInH1('Select One Record From Accounts Table');
+    StringFunctions::printThisInH3('Record with id = 1 is selected');
+    $record = accounts::findOne(1);
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    StringFunctions::printThisInH1('Select All Records From Accounts Table');
+    $record = accounts::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    $id=59;
+    StringFunctions::printThisInH1('Delete Record From Accounts Table');
+    $record = new account();
+    $record->delete($id);
+    StringFunctions::printThisInH3('Record with id = '.$id.' is deleted');
+    $record = accounts::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    StringFunctions::printThisInH1('Insert New Record in Accounts Table');
+    $obj = new Account;
+    $obj->save('');
+    $record = accounts::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    $id=60;
+    StringFunctions::printThisInH1('Update New Record in Accounts Table');
+    StringFunctions::printThisInH3('Updated lname = Nayak where id = '.$id);
+    $obj->save($id);
+    $record = accounts::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    /**************************************TODOS TABLE***********************************/
+    StringFunctions::printThisInH1('Select One Record From Todos Table');
+    StringFunctions::printThisInH3('Record with id = 1 is selected');
+    $record = todos::findOne(1);
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    StringFunctions::printThisInH1('Select All Records From Todos Table');
+    $record = todos::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    $id=36;
+    StringFunctions::printThisInH1('Delete Record From Todos Table with id = ' .$id);
+    $record = new todo();
+    $record->delete($id);
+    StringFunctions::printThisInH3('Record with id = '.$id.' is deleted');
+    $record = todos::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    StringFunctions::printThisInH1('Insert New Record in Todos Table');
+    $obj = new todo();
+    $obj->save('');
+    $record = todos::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+
+    $id=37;
+    StringFunctions::printThisInH1('Update New Record in Todos Table');
+    StringFunctions::printThisInH3('Updated owneremail = tn76@njit.edu where id = '.$id);
+    $obj->save($id);
+    $record = todos::findAll();
+    $record = htmlTable::makeTable($record);
+    StringFunctions::horizontalRule();
+?>
+
+
