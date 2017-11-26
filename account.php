29309e31b579c8d2e5badde8bde26034bf0fa868
Tanuka Nayak <tanuka1990@gmail.com>
2017-11-26 16:10:07 -0500

    Update account.php

--- account.php
+++ account.php
@@ -1,15 +1,16 @@
 <?php
     class account extends model {
-        public $email = 'email';
-        public $fname = 'fname';
-        public $lname = 'lname';
-        public $phone = 'phone';
-        public $birthday = 'birthday';
-        public $gender = 'gender';
-        public $password = 'password';
-        protected static $dataToInsert = array('tn76@njit.edu','Tanuka','N','9876543210','10-15-1990','female','12345');
-        public static $tableName = 'accounts';
-        public static $columnToUpdate = 'lname';
-        public static $updateData = 'Nayak';
+        public $id;
+        public $email;
+        public $fname;
+        public $lname;
+        public $phone;
+        public $birthday;
+        public $gender;
+        public $password;
+
+        public function __construct() {
+            $this->tableName = 'accounts';
+        }
     }
 ?>
